56b54e1909b97eb0c4f5d12b9503251b9b7bdda7
anandctx <anandctx@outlook.com>
2023-05-31 02:00:25 +0000

    a

--- main.tf
+++ main.tf
@@ -12,3 +12,8 @@ resource "azurerm_resource_group" "admin1" {
   name = "admin1-rg-1"
   location = "north europe"
 }
+
+resource "azurerm_resource_group" "admin2" {
+  name = "admin2-rg-2"
+  location = "north europe"
+}
